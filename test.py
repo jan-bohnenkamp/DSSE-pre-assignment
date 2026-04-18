02c24f4cf0a4fbd39841f3614908d74fee76a451
jan-ITruns <bohnenkamp.jan@gmail.com>
2026-04-18 13:12:55 +0200

    Test.py: Added a function for executing git commands in python

--- test.py
+++ test.py
@@ -1,2 +1,21 @@
-print("Hello")
+import subprocess
+from collections import defaultdict
+
+def run_git_command(command):
+    result = subprocess.run(
+        args=command, 
+        stdout=subprocess.PIPE, 
+        text=True
+    )
+    return result.stdout
+
+
+def count_total_commits():
+    output = run_git_command(["git", "log", "--oneline"])
+    
+
+
+print(run_git_command(["git", "status"]))
+
+
 
