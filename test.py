ad9f7f6234f4e8c21776a970d4192b574436b759
Jan Bohnenkamp <bohnenkamp.jan@gmail.com>
2026-04-18 13:30:09 +0200

    Test.py: Added a function to count commits per author

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import subprocess
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 def run_git_command(command):
     result = subprocess.run(
@@ -12,10 +12,12 @@ def run_git_command(command):
 
 def count_total_commits():
     output = run_git_command(["git", "log", "--oneline"])
+    return len(output.strip().split("\n"))
     
+def commits_per_author():
+    output = run_git_command(["git", "log", "--pretty=format:%an"])
+    print(output)
+    return Counter(output.split("\n"))
 
 
-print(run_git_command(["git", "status"]))
-
-
-
+print(commits_per_author())
